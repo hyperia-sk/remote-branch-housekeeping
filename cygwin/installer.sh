694327c24abc84e47fad78477d442a4ae15e7826
Lukas Mestan <lukas.mestan@hyperia.sk>
2017-08-15 09:45:56 +0200

    cygwin installer / uninstaller

---
+++ cygwin/installer.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+echo "Cygwin Installer - remote-branch-housekeeping"
+chmod 0755 remote-branch-housekeeping && cp remote-branch-housekeeping /usr/local/bin/remote-branch-housekeeping
+echo -e "...\ndone"
