23221cc0a97718d4b0d62efa0c18fec602a40a16
Lukas Mestan <lukas.mestan@hyperia.sk>
2017-10-06 13:26:56 +0200

    update usage

--- tests/commands_test.sh
+++ tests/commands_test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+export GREP="grep"
 . tests/assert.sh -v
 
 src="./remote-branch-housekeeping"
 
 assert_raises "$src" 0
-assert_contains "$src -h" "Git Remote Branch Housekeeping" 1
-assert_contains "$src" "Fetching branches" 127
-assert_contains "$src" "Deleting branches" 127
-assert_contains "$src" "Conditions" 127
-assert_contains "$src" "commits older than 14 days" 127
-assert_contains "$src" "ranch fully merged into origin master" 127
+assert_contains "$src -h" "Branch Housekeeping" 
+assert_contains "$src -a" "Fetching branches" 
+assert_contains "$src -a" "Deleting branches"
+assert_contains "$src -a" "Conditions"
+assert_contains "$src -a" "commits older than 14 days"
+assert_contains "$src -a" "branch fully merged into origin master"
 
 assert_end
